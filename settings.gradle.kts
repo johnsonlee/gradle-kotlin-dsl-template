cdb7b17f645ca976b316133fcae78b96e52e7e2c
johnsonlee <g.johnsonlee@gmail.com>
2021-10-06 21:03:53 +0800

    init

---
+++ settings.gradle.kts
@@ -0,0 +1,12 @@
+pluginManagement {
+    repositories {
+        mavenCentral()
+        google()
+        gradlePluginPortal()
+    }
+}
+
+include(":app")
+include(":compiler")
+include(":framework")
+include(":plugin")
